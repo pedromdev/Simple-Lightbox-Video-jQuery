02f2f8229590ed058d561ecf47cadc700dda64c4
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-07-26 22:16:57 -0300

    Trocando classes e IDs para evitar conflitos

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,18 +1,18 @@
-$('.link-lightbox').on('click', function() {
+$('.slvj-link-lightbox').on('click', function() {
 	if (window.innerHeight > 540) {
 		var margintop = (window.innerHeight - 540) / 2;
 	}
 
-	var ifr = '<iframe src="" width="640" height="480" id="video-embed" style="border:0;"></iframe>';
-	var close = '<div id="close-icon"></div>';
-	var lightbox = '<div class="lightbox" style="margin-top:' + margintop + 'px">';
-	var back = '<div id="back-lightbox">';
-	var bclo = '<div id="background-close"></div>';
-	var win = '<div id="window">';
+	var ifr = '<iframe src="" width="640" height="480" id="slvj-video-embed" style="border:0;"></iframe>';
+	var close = '<div id="slvj-close-icon"></div>';
+	var lightbox = '<div class="slvj-lightbox" style="margin-top:' + margintop + 'px">';
+	var back = '<div id="slvj-back-lightbox">';
+	var bclo = '<div id="slvj-background-close"></div>';
+	var win = '<div id="slvj-window">';
 	var end = '</div></div></div>';
 
 	$('body').append(win + bclo + back + lightbox + close + ifr + end);
-	$('#window').hide();
+	$('#slvj-window').hide();
 
 	if ($(this).data('videosite') == "youtube") {
 		var url = 'http://www.youtube.com/embed/' + $(this).data('videoid') + '?autoplay=1';
@@ -20,17 +20,17 @@ $('.link-lightbox').on('click', function() {
 		var url = 'http://player.vimeo.com/video/' + $(this).data('videoid') + '?autoplay=1';
 	}
 
-	$('#window').fadeIn();
-	$('#video-embed').attr('src', url);
+	$('#slvj-window').fadeIn();
+	$('#slvj-video-embed').attr('src', url);
 
-	$('#close-icon').on('click', function() {
-		$('#window').fadeOut(1000, function(){
+	$('#slvj-close-icon').on('click', function() {
+		$('#slvj-window').fadeOut(300, function(){
 			$(this).remove();
 		});
 	});
 
-	$('#background-close').on('click', function() {
-		$('#window').fadeOut(1000, function(){
+	$('#slvj-background-close').on('click', function() {
+		$('#slvj-window').fadeOut(300, function(){
 			$(this).remove();
 		});
 	});
@@ -38,13 +38,13 @@ $('.link-lightbox').on('click', function() {
 
 $(document).on('keyup', function(e){
 	if (e.keyCode == 27) {
-		$('#window').fadeOut(1000, function(){
+		$('#slvj-window').fadeOut(300, function(){
 			$(this).remove();
 		});
 	}
 });
 
-$(document).on('mouseover', function(){
+$(window).resize(function(){
 	var margintop = (window.innerHeight - 540) / 2;
-	$('.lightbox').attr('style', 'margin-top:' + margintop + 'px');
+	$('.slvj-lightbox').attr('style', 'margin-top:' + margintop + 'px');
 });
